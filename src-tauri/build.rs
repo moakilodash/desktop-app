bd7ae276dcfdedcc4ba0b50ef5b4678586eb67ba
Antonio <bitcynic@protonmail.com>
2024-07-15 15:11:06 +0200

    RGB Lightning Node integration

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,26 @@
+use std::process::Command;
+use std::fs;
+use std::path::PathBuf;
+
 fn main() {
+  let output = Command::new("make")
+      .current_dir("../")
+      .output()
+      .expect("Failed to run make");
+
+  if !output.status.success() {
+      let stderr = String::from_utf8_lossy(&output.stderr);
+      fs::write("../build_error.log", stderr.as_bytes())
+          .expect("Failed to write build error log");
+      panic!("Make command failed. Check build_error.log for details.");
+  }
+
+  let executable_path = PathBuf::from("../bin/rgb-lightning-node");
+  let executable_path_absolute = fs::canonicalize(&executable_path)
+        .expect("Failed to get absolute path of executable");
+
+  println!("cargo:rerun-if-changed={}", executable_path.display());
+  println!("cargo:rustc-env=EXECUTABLE_PATH={}", executable_path_absolute.display());
+
   tauri_build::build()
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,42 +1,104 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::process::Command;
-use std::{env, thread};
+use serde::{Deserialize, Serialize};
+use std::{env, fs, sync::{Arc, Mutex}};
+use std::path::{Path, PathBuf};
+use fs_extra::dir::create_all;
+use std::process::{Child, Command};
+use tauri::Manager;
 
-fn run_docker_command(network: &str, datapath: &str, rpc_connection_url: &str) -> std::process::Child {
-    Command::new("sudo")
-        .arg("docker")
-        .arg("run")
-        .arg("--name=kaleidoswap_node") 
-        .arg("--network=regnet")
-        .arg("--publish=3001:3001") 
-        .arg("--publish=9736:9736") 
-        .arg("rgb-lightning-node:latest")
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    network: String,
+    datapath: String,
+    rpc_connection_url: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            network: "regtest".to_string(),
+            datapath: "../bin/dataldk".to_string(),
+            rpc_connection_url: "user:password@localhost:18443".to_string(),
+        }
+    }
+}
+
+fn load_config(config_path: &Path) -> Config {
+    if config_path.exists() {
+        let config_content = fs::read_to_string(config_path).expect("Failed to read config file");
+        serde_yaml::from_str(&config_content).expect("Failed to parse config file")
+    } else {
+        let default_config = Config::default();
+        let config_yaml = serde_yaml::to_string(&default_config).expect("Failed to serialize default config");
+        fs::write(config_path, config_yaml).expect("Failed to write default config file");
+        default_config
+    }
+}
+
+fn run_rgb_lightning_node(network: &str, datapath: &str, rpc_connection_url: &str) -> Child {
+    let mut executable_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    executable_path.push("../bin/rgb-lightning-node");
+
+    Command::new(executable_path)
         .args(&[rpc_connection_url, datapath])
         .args(&["--daemon-listening-port", "3001"])
         .args(&["--ldk-peer-listening-port", "9736"])
         .args(&["--network", network])
         .spawn()
-        .expect("Failed to start Docker process")
-        
-}
-
-fn init_process(network: String, datapath: String, rpc_connection_url: String) {
-    thread::spawn(move || {
-        let child = run_docker_command(&network, &datapath, &rpc_connection_url);
-        println!("Docker process started with pid: {}", child.id());
-    });
+        .expect("Failed to start rgb-lightning-node process")
 }
 
 fn main() {
-    let network = env::var("NETWORK").unwrap_or_else(|_| "regtest".to_string());
-    let datapath = env::var("DATAPATH").unwrap_or_else(|_| "/tmp/kaleidoswap/dataldk1/".to_string());
-    let rpc_connection_url = env::var("RPC_CONNECTION_URL").unwrap_or_else(|_| "user:password@host.docker.internal:18443".to_string());
+    let mut config_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    config_path.push("../bin/config.yaml");
 
-    // init_process(network, datapath, rpc_connection_url);
+    let config = load_config(&config_path);
+
+    let mut data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    data_path.push(&config.datapath);
+
+    if !data_path.exists() {
+        create_all(&data_path, true).expect("Failed to create dataldk directory");
+    }
+
+    let network = config.network;
+    let datapath = data_path.to_str().unwrap().to_string();
+    let rpc_connection_url = config.rpc_connection_url;
+
+    println!("Network: {}", network);
+    println!("Data Path: {}", datapath);
+    println!("RPC Connection URL: {}", rpc_connection_url);
+
+    // Use Arc and Mutex to share the child process reference safely
+    let child_process = Arc::new(Mutex::new(Some(run_rgb_lightning_node(
+        &network,
+        &datapath,
+        &rpc_connection_url,
+    ))));
+
+    let child_process_clone = Arc::clone(&child_process);
 
     tauri::Builder::default()
+        .on_window_event(move |event| {
+            if let tauri::WindowEvent::CloseRequested { .. } = event.event() {
+                if let Some(mut child) = child_process_clone.lock().unwrap().take() {
+                    child.kill().expect("Failed to kill child process");
+                }
+            }
+        })
+        .setup(move |app| {
+            let handle = app.handle();
+            let child_process_clone = Arc::clone(&child_process);
+            app.listen_global("tauri://exit", move |_| {
+                if let Some(mut child) = child_process_clone.lock().unwrap().take() {
+                    child.kill().expect("Failed to kill child process");
+                }
+                handle.exit(0);
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
