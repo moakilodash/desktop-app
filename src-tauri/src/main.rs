d7b6d9a736ce2e45b841b70b8e9c7af5c41dd7ab
bitwalt <walterm21@proton.me>
2024-10-22 10:07:22 +0200

    from multithread to multiprocess

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,173 +4,27 @@
 use db::Account;
 use dotenv::dotenv;
 use fs_extra::dir::create_all;
-use rgb_node::NodeThread;
-use serde::{Deserialize, Serialize};
-use std::path::{Path, PathBuf};
-use std::{
-    env, fs,
-    sync::{Arc, Mutex},
-};
-
+use rgb_node::NodeProcess;
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
 use tauri::{Manager, Window};
 
 mod db;
 mod rgb_node;
 
-// #[derive(Serialize, Deserialize, Debug)]
-// struct Account {
-//     name: String,
-//     datapath: String,
-// }
-
-// #[derive(Serialize, Deserialize, Debug)]
-// struct Config {
-//     network: String,
-//     datapath: String,
-//     rpc_connection_url: String,
-//     accounts: Vec<Account>,
-// }
-//
-// impl Default for Config {
-//     fn default() -> Self {
-//         Self {
-//             network: "regtest".to_string(),
-//             datapath: "../bin/dataldk".to_string(),
-//             rpc_connection_url: "user:password@localhost:18443".to_string(),
-//             accounts: vec![],
-//         }
-//     }
-// }
-
-// fn load_config(config_path: &Path) -> Config {
-//     if config_path.exists() {
-//         let config_content = fs::read_to_string(config_path).expect("Failed to read config file");
-//         serde_yaml::from_str(&config_content).expect("Failed to parse config file")
-//     } else {
-//         // Crea la directory se non esiste
-//         if let Some(parent) = config_path.parent() {
-//             if !parent.exists() {
-//                 create_all(parent, true).expect("Failed to create config directory");
-//             }
-//         }
-//         let default_config = Config::default();
-//         let config_yaml =
-//             serde_yaml::to_string(&default_config).expect("Failed to serialize default config");
-//         fs::write(config_path, config_yaml).expect("Failed to write default config file");
-//         default_config
-//     }
-// }
-
-// fn run_rgb_lightning_node(
-//     network: &str,
-//     datapath: &str,
-//     rpc_connection_url: &str,
-// ) -> Option<Child> {
-//     let mut executable_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-//     executable_path.push("../bin/rgb-lightning-node");
-//
-//     if executable_path.exists() {
-//         Some(
-//             Command::new(executable_path)
-//                 .args(&[rpc_connection_url, datapath])
-//                 .args(&["--daemon-listening-port", "3001"])
-//                 .args(&["--ldk-peer-listening-port", "9736"])
-//                 .args(&["--network", network])
-//                 .spawn()
-//                 .expect("Failed to start rgb-lightning-node process"),
-//         )
-//     } else {
-//         println!("rgb-lightning-node executable not found.");
-//         None
-//     }
-// }
-
-// #[tauri::command]
-// fn is_wallet_init() -> bool {
-//     let data_path = match get_config() {
-//         Ok(config) => PathBuf::from(config.datapath),
-//         Err(_) => return false,
-//     };
-//
-//     if !data_path.exists() {
-//         return false;
-//     }
-//
-//     let wallet_path = data_path.join("mnemonic");
-//
-//     wallet_path.exists()
-// }
-
-// #[tauri::command]
-// fn get_config() -> Result<Config, String> {
-//     let mut config_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-//     config_path.push("../bin/config.yaml");
-//     if config_path.exists() {
-//         let config_content = match fs::read_to_string(config_path) {
-//             Ok(content) => content,
-//             Err(_) => return Err("Failed to read config file".to_string()),
-//         };
-//         match serde_yaml::from_str(&config_content) {
-//             Ok(config) => Ok(config),
-//             Err(_) => Err("Failed to parse config file".to_string()),
-//         }
-//     } else {
-//         Err("Config file not found".to_string())
-//     }
-// }
-//
-// #[tauri::command]
-// fn write_config(config: Config) -> Result<(), String> {
-//     let mut config_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-//     config_path.push("../bin/config.yaml");
-//
-//     let config_yaml = serde_yaml::to_string(&config).expect("Failed to serialize default config");
-//     match fs::write(config_path, config_yaml) {
-//         Ok(_) => Ok(()),
-//         Err(_) => Err("Failed to write config file".to_string()),
-//     }
-// }
-
 fn main() {
     dotenv().ok();
-    // let use_local_bin = env::var("BUILD_AND_RUN_RGB_LIGHTNING_NODE")
-    //     .unwrap_or_else(|_| "true".to_string())
-    //     == "true";
-    //
-    // let mut config_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    // config_path.push("../bin/config.yaml");
-    //
-    // let config = load_config(&config_path);
-    //
-    // let mut data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    // data_path.push(&config.datapath);
-    //
-    // if !data_path.exists() {
-    //     create_all(&data_path, true).expect("Failed to create dataldk directory");
-    // }
-    //
-    // let network = config.network.clone();
-    // let datapath = data_path.to_str().unwrap().to_string();
-    // let rpc_connection_url = config.rpc_connection_url.clone();
-    //
-    let node_thread = Arc::new(Mutex::new(NodeThread::new()));
-    //
-    // if use_local_bin {
-    //     let node_thread_clone = Arc::clone(&node_thread);
-    //     node_thread_clone
-    //         .lock()
-    //         .unwrap()
-    //         .start(network, datapath, rpc_connection_url);
-    // }
+    
+    let node_process = Arc::new(Mutex::new(NodeProcess::new()));
 
     tauri::Builder::default()
-        .manage(Arc::clone(&node_thread))
+        .manage(Arc::clone(&node_process))
         .on_window_event({
-            let node_thread = Arc::clone(&node_thread);
+            let node_process = Arc::clone(&node_process);
             move |event| {
                 if let tauri::WindowEvent::CloseRequested { .. } = event.event() {
-                    let mut node_thread = node_thread.lock().unwrap();
-                    node_thread.stop();
+                    let mut node_process = node_process.lock().unwrap();
+                    node_process.stop();
                 }
             }
         })
@@ -184,7 +38,6 @@ fn main() {
             close_splashscreen,
             get_accounts,
             insert_account,
-            // write_config,
             start_node,
             stop_node
         ])
@@ -204,7 +57,7 @@ async fn close_splashscreen(window: Window) {
 
 #[tauri::command]
 fn start_node(
-    node_thread: tauri::State<Arc<Mutex<NodeThread>>>,
+    node_process: tauri::State<Arc<Mutex<NodeProcess>>>,
     network: String,
     datapath: String,
     rpc_connection_url: String,
@@ -214,16 +67,16 @@ fn start_node(
 
     let datapath = executable_dir.join(datapath).to_str().unwrap().to_string();
 
-    let mut node_thread = node_thread.lock().unwrap();
-    node_thread.start(network, datapath, rpc_connection_url);
+    let mut node_process = node_process.lock().unwrap();
+    node_process.start(network, datapath, rpc_connection_url);
     Ok(())
 }
 
 #[tauri::command]
-fn stop_node(node_thread: tauri::State<Arc<Mutex<NodeThread>>>) -> Result<(), String> {
+fn stop_node(node_process: tauri::State<Arc<Mutex<NodeProcess>>>) -> Result<(), String> {
     println!("Locking mutex");
-    let mut node_thread = node_thread.lock().unwrap();
-    node_thread.stop();
+    let mut node_process = node_process.lock().unwrap();
+    node_process.stop();
     println!("Node stopped");
     Ok(())
 }
--- src-tauri/src/rgb_node.rs
+++ src-tauri/src/rgb_node.rs
@@ -1,35 +1,55 @@
 use std::path::PathBuf;
 use std::process::{Child, Command};
-use std::sync::{Arc, Mutex};
-use std::thread::{self, JoinHandle};
+use std::sync::mpsc::{channel, Sender, Receiver};
 use std::time::Duration;
+use std::thread;
+use std::sync::{Arc, Mutex};
 
-pub struct NodeThread {
-    child_process: Arc<Mutex<Option<Child>>>,
-    thread_handle: Option<JoinHandle<()>>,
+pub struct NodeProcess {
+    child_process: Option<Child>,
+    control_sender: Sender<ControlMessage>,
+    control_receiver: Arc<Mutex<Receiver<ControlMessage>>>,
 }
 
-impl NodeThread {
+enum ControlMessage {
+    Stop,
+}
+
+impl NodeProcess {
     pub fn new() -> Self {
-        NodeThread {
-            child_process: Arc::new(Mutex::new(None)),
-            thread_handle: None,
+        let (tx, rx) = channel();
+        NodeProcess {
+            child_process: None,
+            control_sender: tx,
+            control_receiver: Arc::new(Mutex::new(rx)),
         }
     }
 
     pub fn start(&mut self, network: String, datapath: String, rpc_connection_url: String) {
-        let child_process = Arc::clone(&self.child_process);
+        let rx = Arc::clone(&self.control_receiver);
 
-        let handle = thread::spawn(move || {
-            let process = run_rgb_lightning_node(&network, &datapath, &rpc_connection_url);
-            if let Some(child) = process {
-                *child_process.lock().unwrap() = Some(child);
-
+        std::thread::spawn(move || {
+            let mut process = run_rgb_lightning_node(&network, &datapath, &rpc_connection_url);
+            
+            if let Some(mut child) = process {
                 loop {
-                    thread::sleep(Duration::from_secs(5));
-                    if let Some(child_ref) = child_process.lock().unwrap().as_mut() {
-                        if let Ok(Some(status)) = child_ref.try_wait() {
+                    if let Ok(ControlMessage::Stop) = rx.lock().unwrap().try_recv() {
+                        println!("Stopping the process...");
+                        let _ = child.kill();
+                        let _ = child.wait();
+                        break;
+                    }
+
+                    match child.try_wait() {
+                        Ok(Some(status)) => {
                             println!("Process finished with status: {:?}", status);
+                            break;
+                        }
+                        Ok(None) => {
+                            thread::sleep(Duration::from_secs(5));
+                        }
+                        Err(e) => {
+                            println!("Error waiting for child process: {:?}", e);
                             break;
                         }
                     }
@@ -38,24 +58,11 @@ impl NodeThread {
                 println!("Failed to start the rgb-lightning-node daemon.");
             }
         });
-
-        self.thread_handle = Some(handle);
     }
 
     pub fn stop(&mut self) {
-        if let Some(_) = self.thread_handle.take() {
-            println!("handle found...");
-            if let Some(mut child) = self.child_process.lock().unwrap().take() {
-                println!("child found");
-                let _ = child.kill();
-                println!("killed child");
-                let _ = child.wait();
-                println!("waited for child");
-            }
-
-            self.thread_handle = None;
-        } else {
-            println!("Thread handle not found.");
+        if let Err(e) = self.control_sender.send(ControlMessage::Stop) {
+            println!("Failed to send stop signal: {:?}", e);
         }
     }
 }
