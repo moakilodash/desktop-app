e1699419ed8bf7e74db4ab7004db558bd1dd83b3
bitwalt <walterm21@proton.me>
2024-11-13 15:32:58 +0100

    minor ui fixes and lsp default url

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -4,11 +4,13 @@ export interface NetworkDefaults {
   proxy_endpoint: string
   daemon_listening_port: string
   ldk_peer_listening_port: string
+  default_lsp_url: string
 }
 
 export const NETWORK_DEFAULTS: Record<string, NetworkDefaults> = {
   mainnet: {
     daemon_listening_port: '3001',
+    default_lsp_url: 'https://api.kaleidoswap.com/',
     indexer_url: '127.0.0.1:50001',
     ldk_peer_listening_port: '9735',
     proxy_endpoint: 'rpc://127.0.0.1:3000/json-rpc',
@@ -16,6 +18,7 @@ export const NETWORK_DEFAULTS: Record<string, NetworkDefaults> = {
   },
   regtest: {
     daemon_listening_port: '3001',
+    default_lsp_url: 'https://api.regtest.kaleidoswap.com/',
     indexer_url: 'electrum.rgbtools.org:50041',
     ldk_peer_listening_port: '9735',
     proxy_endpoint: 'rpcs://proxy.iriswallet.com/0.2/json-rpc',
@@ -23,6 +26,7 @@ export const NETWORK_DEFAULTS: Record<string, NetworkDefaults> = {
   },
   signet: {
     daemon_listening_port: '3001',
+    default_lsp_url: 'https://api.signet.kaleidoswap.com/',
     indexer_url: '127.0.0.1:60601',
     ldk_peer_listening_port: '9735',
     proxy_endpoint: 'rpc://127.0.0.1:3000/json-rpc',
@@ -30,6 +34,7 @@ export const NETWORK_DEFAULTS: Record<string, NetworkDefaults> = {
   },
   testnet: {
     daemon_listening_port: '3001',
+    default_lsp_url: 'https://api.testnet.kaleidoswap.com/',
     indexer_url: 'ssl://electrum.iriswallet.com:50013',
     ldk_peer_listening_port: '9735',
     proxy_endpoint: 'rpcs://proxy.iriswallet.com/0.2/json-rpc',
