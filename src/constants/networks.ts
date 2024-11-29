e5b7bb50e7be311f8655ab811e4166eaa9c62fbe
bitwalt <walterm21@proton.me>
2024-11-29 16:28:24 +0100

    minor fixes

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -29,7 +29,7 @@ export const NETWORK_DEFAULTS: Record<string, NetworkDefaults> = {
     default_lsp_url: 'https://api.signet.kaleidoswap.com/',
     indexer_url: 'electrum.signet.kaleidoswap.com:60601',
     ldk_peer_listening_port: '9735',
-    proxy_endpoint: 'rpc://proxy.signet.kaleidoswap.com:3000/json-rpc',
+    proxy_endpoint: 'rpcs://proxy.signet.kaleidoswap.com:3000/json-rpc',
     rpc_connection_url:
       'user:default_password@bitcoind.signet.kaleidoswap.com:38332',
   },
